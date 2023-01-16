9e6b698e9d37dad19e1fdb2baf3955b49563e573
Michael Wilson <pianoplayer868@hotmail.com>
2023-01-16 12:25:47 -0800

    Added a crude visual stepper

--- Arrays and Strings/index.js
+++ Arrays and Strings/index.js
@@ -1,18 +1,51 @@
+word = new Array();
+
+const sleep = ms => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const highlightCharacter = id => {
+    return sleep(1000).then(v => {
+        document.querySelector(`#${id}`).classList.add('highlight');
+    });
+}
+
 // Pure function as we are not modifying the contents and the return is the same
-const isUnique = word => {
+const isUnique = async word => {
     characters = new Set();
-    for(const c of word) {
+    const output = document.querySelector('#is-unique-output');
+    for(let i = 0; i < word.length; i++) {
+        const c = word[i];
+        const id = `${c}-${i}`;
+        await highlightCharacter(id);
         if(characters.has(c)) {
-            return false;
+            output.innerHTML = 'Is unique: False';
         }
+
         characters.add(c);
     }
 
-    return true;
+    output.innerHTML = 'Is unique: True';
 }
 
-function checkIsUnique() {
+async function checkIsUnique() {
     const word = document.querySelector('#is-unique-input').value;
-    const unique = isUnique(word);
-    document.querySelector('#is-unique-output').innerHTML = `Is unique: ${unique ? 'True' : 'False'}`;
+    
+    // display the words 
+    let visualContainer = document.querySelector('#is-unique-visual');
+    while (visualContainer.firstChild) {
+        visualContainer.firstChild.remove();
+    }
+
+    for (let i = 0; i < word.length; i++) {
+        const span = document.createElement('span');
+        const c = word[i];
+        const id = `${c}-${i}`;
+        span.id = id;
+        span.innerHTML = c;
+        span.className = 'character';
+        visualContainer.appendChild(span);
+    }
+
+    await isUnique(word);
 }
